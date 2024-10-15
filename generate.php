d7fa98a6960cf0f731bf566a539a02f94e4a2865
Titus Breede <smite.t@gmx.de>
2024-10-15 13:41:14 +0200

    Initial commit

---
+++ generate.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . "/kirby/bootstrap.php";
+
+$customRoutes = [
+	[
+		"path"  => "feed-de.xml",
+		"route" => "de/feed",
+	],
+	[
+		"path"  => "feed-en.xml",
+		"route" => "en/feed",
+	],
+];
+
+$kirby = kirby();
+$staticSiteGenerator = new D4L\StaticSiteGenerator($kirby);
+$staticSiteGenerator->setCustomRoutes($customRoutes);
+/*removed because addes unnessecary complexity*/
+/*$staticSiteGenerator->setIgnoreUntranslatedPages(true);*/
+$files = $staticSiteGenerator->generate("./build");
